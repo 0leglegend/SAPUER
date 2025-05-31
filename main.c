e2d485835feed73dc578fe0249b4c93288f50fea
0leglegend <micelubim@gmail.com>
2025-05-31 21:10:34 +0300

    done

--- main.c
+++ main.c
@@ -8,22 +8,25 @@ void main(int argc,char *argv[]){
   srand(time(NULL));
   int row = ROW, col = COL;
   int ch,cur_row = 0, cur_col = 0;
-  int len_ans = 2;
-  Point * ans = (Point *)malloc(len_ans * sizeof(Point));
-  for (int i = 0; i < len_ans; i++){
-    (ans + i)->x = rand() % COL;
-    (ans + i)->y = rand() % ROW;
-  }
-  for (int i = 0; i <= ROW;){
-    for (int j = 0; j < COL; j++){
-      printw(".");
+  do{
+    int len_ans = 2;
+    Point * ans = (Point *)malloc(len_ans * sizeof(Point));
+    for (int i = 0; i < len_ans; i++){
+      (ans + i)->x = rand() % COL;
+      (ans + i)->y = rand() % ROW;
     }
-    move(i++,0);
-  }
-  move(0,0);
-  refresh();
-  while((ch = getch()) != 27)
+    for (int i = 0; i <= ROW;){
+      for (int j = 0; j < COL; j++){
+        printw(".");
+      }
+      move(i++,0);
+    }
+    move(0,0);
+    refresh();
+    
     move_select(ch, &cur_row, &cur_col, row, col, ans, len_ans);
+    if(ch = 'r') continue;
+  }while((ch = getch()) != 27);
   endwin(); 
   return;
 }
